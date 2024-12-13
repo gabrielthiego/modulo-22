29680d4786da74a806be977983a1c2f749488799
Gabriel Thiego <gabrielthiego@gmail.com>
2024-12-12 22:23:43 -0300

    exercicios/modulo-4

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
 const { defineConfig } = require("cypress");
+const createMochawesomeReporter = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      createMochawesomeReporter(on);  
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          const debuggingPort = launchOptions.args.find(
+            (arg) => arg.slice(0, 23) === '--remote-debugging-port',
+          );
+          setDebuggingPort(debuggingPort.split('='));
+        }
+        return launchOptions;
+      });
+
+      on('task', {
+        tabNavigation
+      });
+
     },
+    env: {
+      MY_ENV: process.env.MY_ENV,
+      ebacStoreVersion: "v2"  
+    }
   },
 });
