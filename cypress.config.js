9a2a56d6cbc425a22bfb565db9e4521bcbc308da
Gabriel Thiego <gabrielthiego@gmail.com>
2025-01-27 20:30:14 -0300

    git actions 1

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,29 @@ module.exports = defineConfig({
           const debuggingPort = launchOptions.args.find(
             (arg) => arg.slice(0, 23) === '--remote-debugging-port',
           );
-          setDebuggingPort(debuggingPort.split('='));
+          setDebuggingPort(debuggingPort.split('='));const { defineConfig } = require("cypress");
+          const createMochawesomeReporter = require('cypress-mochawesome-reporter/plugin');
+          
+          module.exports = defineConfig({
+            e2e: {
+              baseUrl: 'http://lojaebac.ebaconline.art.br/',
+              setupNodeEvents(on, config) {
+                createMochawesomeReporter(on);
+              },
+              env: {
+                MY_ENV: process.env.MY_ENV,
+                ebacStoreVersion: "v2"
+              },
+              reporter: 'cypress-mochawesome-reporter',
+              reporterOptions: {
+                reportDir: 'cypress/reports',
+                overwrite: false,
+                html: true,
+                json: true
+              }
+            },
+          });
+          
         }
         return launchOptions;
       });
